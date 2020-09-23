fcfeb0862687eb52fce2f35242022843ce974c7f
Alpha <alpha0010@users.noreply.github.com>
2020-09-23 17:28:14 -0400

    feat(ios): render pages in background

--- ios/PdfView.swift
+++ ios/PdfView.swift
@@ -16,52 +16,58 @@ class PdfView: UIView {
         guard !frame.isEmpty && !source.isEmpty else {
             return
         }
-        let url = URL(fileURLWithPath: source)
-        guard let pdf = CGPDFDocument(url as CFURL) else {
-            return
-        }
-        guard let pdfPage = pdf.page(at: page.intValue + 1) else {
-            return
-        }
 
-        UIGraphicsBeginImageContextWithOptions(frame.size, true, 0.0)
-        guard let context = UIGraphicsGetCurrentContext() else {
+        let currentFrame = frame
+        DispatchQueue.global().async {
+            let url = URL(fileURLWithPath: self.source)
+            guard let pdf = CGPDFDocument(url as CFURL) else {
+                return
+            }
+            guard let pdfPage = pdf.page(at: self.page.intValue + 1) else {
+                return
+            }
+
+            UIGraphicsBeginImageContextWithOptions(currentFrame.size, true, 0.0)
+            guard let context = UIGraphicsGetCurrentContext() else {
+                UIGraphicsEndImageContext()
+                return
+            }
+            context.saveGState()
+
+            UIColor.white.setFill()
+            context.fill(currentFrame)
+
+            let pageBounds = pdfPage.getBoxRect(.cropBox)
+            let pageHeight: CGFloat;
+            let pageWidth: CGFloat;
+            if pdfPage.rotationAngle % 180 == 90 {
+                pageHeight = pageBounds.width
+                pageWidth = pageBounds.height
+            } else {
+                pageHeight = pageBounds.height
+                pageWidth = pageBounds.width
+            }
+            // Change context coordinate system to pdf coordinates
+            context.translateBy(x: 0.0, y: currentFrame.height)
+            context.scaleBy(x: currentFrame.width / pageWidth, y: -currentFrame.height / pageHeight)
+            context.concatenate(pdfPage.getDrawingTransform(
+                .cropBox,
+                rect: CGRect(x: 0.0, y: 0.0, width: pageWidth, height: pageHeight),
+                rotate: 0,
+                preserveAspectRatio: false
+            ))
+
+            context.interpolationQuality = .high
+            context.setRenderingIntent(.defaultIntent)
+            context.drawPDFPage(pdfPage)
+            let rendered = UIGraphicsGetImageFromCurrentImageContext()
+
+            context.restoreGState()
             UIGraphicsEndImageContext()
-            return
+
+            DispatchQueue.main.async {
+                self.layer.contents = rendered?.cgImage
+            }
         }
-        context.saveGState()
-
-        UIColor.white.setFill()
-        context.fill(frame)
-
-        let pageBounds = pdfPage.getBoxRect(.cropBox)
-        let pageHeight: CGFloat;
-        let pageWidth: CGFloat;
-        if pdfPage.rotationAngle % 180 == 90 {
-            pageHeight = pageBounds.width
-            pageWidth = pageBounds.height
-        } else {
-            pageHeight = pageBounds.height
-            pageWidth = pageBounds.width
-        }
-        // Change context coordinate system to pdf coordinates
-        context.translateBy(x: 0.0, y: frame.height)
-        context.scaleBy(x: frame.width / pageWidth, y: -frame.height / pageHeight)
-        context.concatenate(pdfPage.getDrawingTransform(
-            .cropBox,
-            rect: CGRect(x: 0.0, y: 0.0, width: pageWidth, height: pageHeight),
-            rotate: 0,
-            preserveAspectRatio: false
-        ))
-
-        context.interpolationQuality = .high
-        context.setRenderingIntent(.defaultIntent)
-        context.drawPDFPage(pdfPage)
-        let rendered = UIGraphicsGetImageFromCurrentImageContext()
-
-        context.restoreGState()
-        UIGraphicsEndImageContext()
-
-        layer.contents = rendered?.cgImage
     }
 }
