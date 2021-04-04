99753ebc395a9f06382563ee75c9f95dcbb4b1f3
Alpha <alpha0010@users.noreply.github.com>
2021-04-04 15:32:21 -0400

    chore: update dependencies

--- example/android/app/src/main/java/com/example/reactnativepdfviewer/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativepdfviewer/MainApplication.java
@@ -30,7 +30,6 @@ public class MainApplication extends Application implements ReactApplication {
         // Packages that cannot be autolinked yet can be added manually here, for PdfViewerExample:
         // packages.add(new MyReactNativePackage());
         packages.add(new PdfViewPackage());
-
         return packages;
       }
 
