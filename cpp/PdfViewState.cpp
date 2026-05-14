812f65942a9dd67e468dd914a091f531e25eca67
Alpha <alpha0010@users.noreply.github.com>
2026-05-14 11:51:34 -0400

    fix: faster initial layout via measure guessing

--- cpp/PdfViewState.cpp
+++ cpp/PdfViewState.cpp
@@ -2,13 +2,26 @@
 
 namespace facebook::react {
 
-PdfViewState::PdfViewState() : width(1), height(1) {}
+namespace {
+// Heuristic: guess that new pages will be the same dimensions as the last
+// measured page.
+int defaultWidth = 1;
+int defaultHeight = 1;
+} // namespace
 
-PdfViewState::PdfViewState(int _width, int _height) : width(_width), height(_height) {}
+PdfViewState::PdfViewState() : width(defaultWidth), height(defaultHeight) {}
+
+PdfViewState::PdfViewState(int _width, int _height) : width(_width), height(_height) {
+  defaultWidth = width;
+  defaultHeight = height;
+}
 
 #ifdef RN_SERIALIZABLE_STATE
 PdfViewState::PdfViewState(const PdfViewState& previousState, folly::dynamic data)
-  : width(data["width"].getInt()), height(data["height"].getInt()) {}
+  : width(data["width"].getInt()), height(data["height"].getInt()) {
+  defaultWidth = width;
+  defaultHeight = height;
+}
 
 folly::dynamic PdfViewState::getDynamic() const {
   return folly::dynamic::object("width", width)("height", height);
